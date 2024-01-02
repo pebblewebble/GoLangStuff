4a516d6a3b7750b052c00c4aaf63fbe2f8ac0141
pebblewebble <142425284+pebblewebble@users.noreply.github.com>
2024-01-02 18:00:35 +0800

    2/1/2024 Small random algo advancements
    Added color to the maze, some comments, some random algorithm advancement, just need to loop it and give it a queue to check if it's visited or not

--- maze.go
+++ maze.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+
+	"github.com/fatih/color"
 )
 
 // REFERENCES
@@ -10,10 +12,10 @@ import (
 // https://medium.com/swlh/fun-with-python-1-maze-generator-931639b4fb7e
 
 func main() {
-	width := 21
-	height := 21
+	width := 11
+	height := 11
 	// This program was designed to have and odd width and height
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1; i++ {
 		initializeMap := initialize(width, height)
 		generatedMaze := generateMaze(initializeMap, width, height)
 		visualizeMaze(generatedMaze)
@@ -21,12 +23,14 @@ func main() {
 }
 
 func initialize(height, width int) [][]string {
+	//Creating the matrix
 	gameMap := make([][]string, height)
 	for heightIndex := 0; heightIndex < height; heightIndex++ {
 		gameMap[heightIndex] = make([]string, width)
 		for widthIndex := 0; widthIndex < width; widthIndex++ {
+			//Generate outer walls and cells
 			if heightIndex == 0 || heightIndex == height-1 || widthIndex == 0 || widthIndex == width-1 || heightIndex%2 == 0 || widthIndex%2 == 0 {
-				gameMap[heightIndex][widthIndex] = "*"
+				gameMap[heightIndex][widthIndex] = color.HiBlueString("*")
 			} else {
 				gameMap[heightIndex][widthIndex] = " "
 			}
@@ -42,15 +46,60 @@ func generateStart(height, width int) []int {
 }
 
 func generateMaze(gameMap [][]string, height, width int) [][]string {
-	//
+	//Generates a random odd X,Y starting position but instead of constantly randomly generating a random position each time,
+	//I could just minus 1 from the position that is not odd.
 	startingPosition := generateStart(height, width)
 	for startingPosition[0]%2 == 0 || startingPosition[1]%2 == 0 {
 		startingPosition = generateStart(height, width)
 	}
 	fmt.Println(startingPosition)
 	gameMap[startingPosition[0]][startingPosition[1]] = "X"
+
+	//Able to choose what type of algo in the future
+	gameMap = randomAlgo(gameMap, startingPosition, height, width)
+
 	// walls := [][]int{}
-	// walls = append(walls)
+	// walls = append()
+	return gameMap
+}
+
+func randomAlgo(gameMap [][]string, startingPosition []int, height, width int) [][]string {
+	//Move and remove the wall between each other
+	//Randomize whether to go vertical or horizontal
+	if rand.Intn(2) == 1 {
+		//Vertical
+		fmt.Println("VERTICAL")
+		if rand.Intn(2) == 1 {
+			//Going Up
+			//If it does not go over the maze
+			if startingPosition[0]-2 > 0 {
+				fmt.Println("UP")
+				gameMap[startingPosition[0]-1][startingPosition[1]] = " "
+			}
+		} else {
+			//Going Down
+			if startingPosition[0]+2 < height {
+				fmt.Println("DOWN")
+				gameMap[startingPosition[0]+1][startingPosition[1]] = " "
+			}
+		}
+	} else {
+		//Horizontal
+		//If it does not go over the maze
+		fmt.Println("HORIZONTAL")
+		if rand.Intn(2) == 1 {
+			if startingPosition[1]+2 < width {
+				fmt.Println("RIGHT")
+				gameMap[startingPosition[0]][startingPosition[1]+1] = " "
+			}
+		} else {
+			if startingPosition[1]-2 > 0 {
+				fmt.Println("LEFT")
+				gameMap[startingPosition[0]][startingPosition[1]-1] = " "
+			}
+		}
+
+	}
 	return gameMap
 }
 
